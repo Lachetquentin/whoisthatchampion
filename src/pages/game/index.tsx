4e852be008d0c40cac9d5a57b12ee4ca203376e6
Lachetquentin <lachet.quentin@protonmail.com>
2022-08-20 02:53:13 +0200

    Add Abilities hint + Fix Champion pics to relate latest patch

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -10,7 +10,9 @@ const Game = () => {
   const [guess, setGuess] = useState<{ value: string }>({ value: '' });
   const [dailyChampion, setDailyChampion] = useState<any>(null);
   const [nbTry, setNbTry] = useState<number>(0);
+  const [abilityId, setAbilityId] = useState<number>(0);
   const [hasWin, setHasWin] = useState<boolean>(false);
+  const [showDescription, setShowDescription] = useState<boolean>(false);
 
   useEffect(() => {
     const getChampionInfos = async (dailyInfos: DailyType) => {
@@ -62,7 +64,7 @@ const Game = () => {
           <img
             alt="champion"
             className="h-64 w-64"
-            src={`https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/${dailyChampion.id}.png`}
+            src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${dailyChampion.id}.png`}
           />
         )}
       </div>
@@ -71,7 +73,57 @@ const Game = () => {
         <p className="justify-end flex">{dailyChampion.title}</p>
       ) : null}
 
-      <div className="justify-center items-center flex flex-nowrap">
+      <div className="flex flex-nowrap justify-center items-center p-2 space-x-5 ">
+        {nbTry >= 2 || hasWin ? (
+          <img
+            src={`https://cdn.communitydragon.org/latest/champion/${dailyChampion.id}/ability-icon/q`}
+            alt="champion q-spell"
+            className="h-16 w-16"
+            onClick={() => {
+              setAbilityId(0);
+              setShowDescription(true);
+            }}
+          />
+        ) : null}
+
+        {nbTry >= 3 || hasWin ? (
+          <img
+            src={`https://cdn.communitydragon.org/latest/champion/${dailyChampion.id}/ability-icon/w`}
+            alt="champion w-spell"
+            className="h-16 w-16"
+            onClick={() => {
+              setAbilityId(1);
+              setShowDescription(true);
+            }}
+          />
+        ) : null}
+
+        {nbTry >= 4 || hasWin ? (
+          <img
+            src={`https://cdn.communitydragon.org/latest/champion/${dailyChampion.id}/ability-icon/e`}
+            alt="champion e-spell"
+            className="h-16 w-16"
+            onClick={() => {
+              setAbilityId(2);
+              setShowDescription(true);
+            }}
+          />
+        ) : null}
+
+        {nbTry >= 5 || hasWin ? (
+          <img
+            src={`https://cdn.communitydragon.org/latest/champion/${dailyChampion.id}/ability-icon/r`}
+            alt="champion r-spell"
+            className="h-16 w-16"
+            onClick={() => {
+              setAbilityId(3);
+              setShowDescription(true);
+            }}
+          />
+        ) : null}
+      </div>
+
+      <div className="justify-center items-center flex flex-nowrap p-4">
         <Input
           name="value"
           setForm={setGuess}
@@ -90,6 +142,25 @@ const Game = () => {
         <p className="text-green-400 uppercase font-bold justify-center text-center text-2xl">
           gagné !! c&apos;était bien {dailyChampion.name} !!!
         </p>
+      )}
+
+      {showDescription && (
+        <div className="flex md:flex-col justify-center items-center mt-10">
+          <div className="flex justify-center items-center flex-col w-72 rounded-lg shadow-xl h-auto p-2">
+            <h2 className="text-base mt-2 mx-4 text-gray-400 font-semibold text-center">
+              {/* Chosen Champion Ability Description */}
+              {purgeByName(
+                dailyChampion.spells[abilityId].description,
+                dailyChampion.name
+              )}
+            </h2>
+            <Button
+              className="my-5 w-auto px-8 h-10 bg-blue-600 text-white rounded-md shadow hover:shadow-lg font-semibold"
+              onClick={() => setShowDescription(false)}
+              label="Close"
+            />
+          </div>
+        </div>
       )}
     </>
   );
